d2c828b093ce0a44070bf3987a4d5b8f00868dee
Roosevelt <panta_85.x@hotmail.com>
2020-04-20 12:58:10 -0500

    first commit

---
+++ Assets/Scenes/AudioManagerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerTest : MonoBehaviour
+{
+    [SerializeField] public AudioClip buttonClickSFX;
+    [SerializeField] public AudioClip music1;
+    [SerializeField] public AudioClip music2;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            AudioManager.Instance.PlaySFX(buttonClickSFX, 1);
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            AudioManager.Instance.PlayMusic(music1);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            AudioManager.Instance.PlayMusic(music2);
+        
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            AudioManager.Instance.PlayMusicWithFade(music1);
+        
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            AudioManager.Instance.PlayMusicWithFade(music2);
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            AudioManager.Instance.PlayMusicWithCrossFade(music1, 3.0f);
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            AudioManager.Instance.PlayMusicWithCrossFade(music2, 3.0f);
+    }
+
+}
